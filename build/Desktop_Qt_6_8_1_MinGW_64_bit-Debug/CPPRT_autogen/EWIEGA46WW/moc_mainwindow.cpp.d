2fa6d2473af80f8b4bfa10d147911eb25ffa1253
silver175 <bomben.silver75@gmail.com>
2025-01-17 13:34:27 +0100

    rework ui v1

--- build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/CPPRT_autogen/EWIEGA46WW/moc_mainwindow.cpp.d
+++
@@ -1,2 +0,0 @@
-C:/Users/bombe/Documents/CPPRT/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/CPPRT_autogen/EWIEGA46WW/moc_mainwindow.cpp: C:/Users/bombe/Documents/CPPRT/mainwindow.h \
-  C:/Users/bombe/Documents/CPPRT/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/CPPRT_autogen/moc_predefs.h
